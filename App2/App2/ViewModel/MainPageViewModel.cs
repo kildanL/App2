68b6143d46d7ccd34fc0a414880b569540a98437
kildanL <97839751+kildanL@users.noreply.github.com>
2022-02-12 01:23:59 +0300

    Добавьте файлы проекта.

---
+++ App2/App2/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Newtonsoft.Json;
+
+namespace App2.ViewModel
+{           
+    
+   public class Comments
+   {
+     public int postId { get; set; }
+     public int id { get; set; }
+     public string name { get; set; }
+     public string email { get; set; }
+     public string body { get; set; }
+   } 
+    
+    public class Resp
+    {
+       public List<Comments> Response { get; set; }
+    }
+
+    public class MainPageViewModel : BindableObject
+    { 
+       
+        public ObservableCollection<Comments> SomeData;
+        HttpClient client;
+        private object source;
+
+        public ICommand Fill => new Command(OnRequest);
+   
+        private const string url = "https://jsonplaceholder.typicode.com/comments";
+
+        public object Source
+        {
+            get => source;
+            set
+            {
+                source = value;
+                OnPropertyChanged();
+            }
+            
+        }
+
+       public async void OnRequest()
+        {
+            client = new HttpClient();
+            var result = await client.GetStringAsync(url);
+            SomeData = new ObservableCollection<Comments>(JsonConvert.DeserializeObject <List<Comments>>(result));
+            Source = SomeData;
+            /*var comments = JsonConvert.DeserializeObject<Resp>(result);
+            SomeData = new ObservableCollection<Comments>(comments.Response);*/
+
+        }
+       
+
+    }
+}
---
+++ App2/App2/Views/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using App2.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPageViewModel vm = new MainPageViewModel();
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = vm;
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            
+
+        }
+    }
+}
